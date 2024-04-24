fd8447611e7f285eadfcb16a16ec400fc9f1138a
deaneturner <deaneturner.com@gmail.com>
2024-04-24 19:59:39 -0400

    get mining info retry

--- src/services/bitcoin-rpc.service.ts
+++ src/services/bitcoin-rpc.service.ts
@@ -10,183 +10,183 @@ import { IMiningInfo } from '../models/bitcoin-rpc/IMiningInfo';
 
 @Injectable()
 export class BitcoinRpcService implements OnModuleInit {
-    private blockHeight = 0;
-    private client: RPCClient;
-    private _newBlock$: BehaviorSubject<IMiningInfo> = new BehaviorSubject(
-      undefined,
+  private blockHeight = 0;
+  private client: RPCClient;
+  private _newBlock$: BehaviorSubject<IMiningInfo> = new BehaviorSubject(
+    undefined,
+  );
+  private currentMiningInfo: IMiningInfo;
+  public newBlock$ = this._newBlock$.pipe(
+    filter((block) => block != null),
+    shareReplay({ refCount: true, bufferSize: 1 }),
+  );
+
+  constructor(
+    private readonly configService: ConfigService,
+    private rpcBlockService: RpcBlockService,
+  ) {}
+
+  async onModuleInit() {
+    const url = this.configService.get('BITCOIN_RPC_URL');
+    const user = this.configService.get('BITCOIN_RPC_USER');
+    const pass = this.configService.get('BITCOIN_RPC_PASSWORD');
+    const port = parseInt(this.configService.get('BITCOIN_RPC_PORT'));
+    const timeout = parseInt(this.configService.get('BITCOIN_RPC_TIMEOUT'));
+    const timeoutPollMiningInfo = parseInt(
+      this.configService.get('BITCOIN_POLL_MINING_INFO_TIMEOUT'),
     );
-    private currentMiningInfo: IMiningInfo;
-    public newBlock$ = this._newBlock$.pipe(
-      filter((block) => block != null),
-      shareReplay({ refCount: true, bufferSize: 1 }),
+
+    this.client = new RPCClient({ url, port, timeout, user, pass });
+
+    this.client.getrpcinfo().then(
+      (res) => {
+        console.log('Bitcoin RPC connected');
+      },
+      () => {
+        console.error('Could not reach RPC host');
+      },
     );
 
-    constructor(
-      private readonly configService: ConfigService,
-      private rpcBlockService: RpcBlockService,
-    ) {}
+    if (this.configService.get('BITCOIN_ZMQ_HOST')) {
+      console.log('Using ZMQ');
+      const sock = new zmq.Subscriber();
 
-    async onModuleInit() {
-        const url = this.configService.get('BITCOIN_RPC_URL');
-        const user = this.configService.get('BITCOIN_RPC_USER');
-        const pass = this.configService.get('BITCOIN_RPC_PASSWORD');
-        const port = parseInt(this.configService.get('BITCOIN_RPC_PORT'));
-        const timeout = parseInt(this.configService.get('BITCOIN_RPC_TIMEOUT'));
-        const timeoutPollMiningInfo = parseInt(
-          this.configService.get('BITCOIN_POLL_MINING_INFO_TIMEOUT'),
-        );
+      sock.connectTimeout = 1000;
+      sock.events.on('connect', () => {
+        console.log('ZMQ Connected');
+      });
+      sock.events.on('connect:retry', () => {
+        console.log('ZMQ Unable to connect, Retrying');
+      });
 
-        this.client = new RPCClient({ url, port, timeout, user, pass });
+      sock.connect(this.configService.get('BITCOIN_ZMQ_HOST'));
+      sock.subscribe('rawblock');
+      // Don't await this, otherwise it will block the rest of the program
+      this.listenForNewBlocks(sock);
+      await this.pollMiningInfo();
+    } else {
+      setInterval(this.pollMiningInfo.bind(this), timeoutPollMiningInfo);
+    }
+  }
 
-        this.client.getrpcinfo().then(
-          (res) => {
-              console.log('Bitcoin RPC connected');
-          },
-          () => {
-              console.error('Could not reach RPC host');
-          },
-        );
+  private async listenForNewBlocks(sock: zmq.Subscriber) {
+    for await (const [topic, msg] of sock) {
+      console.log('New Block');
+      await this.pollMiningInfo();
+    }
+  }
 
-        if (this.configService.get('BITCOIN_ZMQ_HOST')) {
-            console.log('Using ZMQ');
-            const sock = new zmq.Subscriber();
+  public async pollMiningInfo() {
+    const miningInfo = await this.getMiningInfo();
+    if (miningInfo != null && miningInfo.blocks > this.blockHeight) {
+      console.log('block height change');
+      this._newBlock$.next(miningInfo);
+      this.blockHeight = miningInfo.blocks;
+    }
+  }
 
-            sock.connectTimeout = 1000;
-            sock.events.on('connect', () => {
-                console.log('ZMQ Connected');
+  private async waitForBlock(blockHeight: number): Promise<IBlockTemplate> {
+    while (true) {
+      await new Promise((r) => setTimeout(r, 100));
+
+      const block = await this.rpcBlockService.getBlock(blockHeight);
+      if (block != null && block.data != null) {
+        console.log('promise loop resolved');
+        return Promise.resolve(JSON.parse(block.data));
+      }
+      console.log('promise loop');
+    }
+  }
+
+  public async getBlockTemplate(blockHeight: number): Promise<IBlockTemplate> {
+    let result: IBlockTemplate;
+    try {
+      const block = await this.rpcBlockService.getBlock(blockHeight);
+
+      if (block != null && block.data != null) {
+        return Promise.resolve(JSON.parse(block.data));
+      } else if (block == null) {
+        if (process.env.NODE_APP_INSTANCE != null) {
+          // There is a unique constraint on the block height so if another process tries to lock, it'll throw
+          try {
+            await this.rpcBlockService.lockBlock(
+              blockHeight,
+              process.env.NODE_APP_INSTANCE,
+            );
+          } catch (e) {
+            result = await this.waitForBlock(blockHeight);
+          }
+        }
+
+        while (!result) {
+          await new Promise((r) => setTimeout(r, 100));
+
+          try {
+            result = await this.client.getblocktemplate({
+              template_request: {
+                rules: ['segwit'],
+                mode: 'template',
+                capabilities: ['serverlist', 'proposal'],
+              },
             });
-            sock.events.on('connect:retry', () => {
-                console.log('ZMQ Unable to connect, Retrying');
-            });
-
-            sock.connect(this.configService.get('BITCOIN_ZMQ_HOST'));
-            sock.subscribe('rawblock');
-            // Don't await this, otherwise it will block the rest of the program
-            this.listenForNewBlocks(sock);
-            await this.pollMiningInfo();
-        } else {
-            setInterval(this.pollMiningInfo.bind(this), timeoutPollMiningInfo);
+            await this.rpcBlockService.saveBlock(
+              blockHeight,
+              JSON.stringify(result),
+            );
+          } catch (e) {
+            console.error('RETRY - getblocktemplate:', e.message);
+          }
         }
+      } else {
+        //wait for block
+        result = await this.waitForBlock(blockHeight);
+      }
+    } catch (e) {
+      console.error('Error getblocktemplate:', e.message);
+      throw new Error('Error getblocktemplate');
     }
+    console.log(`getblocktemplate tx count: ${result.transactions.length}`);
+    return result;
+  }
 
-    private async listenForNewBlocks(sock: zmq.Subscriber) {
-        for await (const [topic, msg] of sock) {
-            console.log('New Block');
-            await this.pollMiningInfo();
-        }
+  public async getMiningInfo(): Promise<IMiningInfo> {
+    let miningInfo: IMiningInfo;
+    let counter = 0;
+    while (!miningInfo && counter < 5) {
+      await new Promise((r) => setTimeout(r, 100));
+      try {
+        miningInfo = await this.client.getmininginfo();
+      } catch (e) {
+        console.error('RETRY - getmininginfo: ', e.message);
+      }
+      counter++;
     }
-
-    public async pollMiningInfo() {
-        const miningInfo = await this.getMiningInfo();
-        if (miningInfo != null && miningInfo.blocks > this.blockHeight) {
-            console.log('block height change');
-            this._newBlock$.next(miningInfo);
-            this.blockHeight = miningInfo.blocks;
-        }
+    if (
+      !this.currentMiningInfo ||
+      miningInfo.blocks !== this.currentMiningInfo.blocks
+    ) {
+      this.currentMiningInfo = miningInfo;
+      console.log(`getMiningInfo: block height ${miningInfo.blocks}`);
     }
+    return miningInfo;
+  }
 
-    private async waitForBlock(blockHeight: number): Promise<IBlockTemplate> {
-        while (true) {
-            await new Promise((r) => setTimeout(r, 100));
-
-            const block = await this.rpcBlockService.getBlock(blockHeight);
-            if (block != null && block.data != null) {
-                console.log('promise loop resolved');
-                return Promise.resolve(JSON.parse(block.data));
-            }
-            console.log('promise loop');
-        }
+  public async SUBMIT_BLOCK(hexdata: string): Promise<string> {
+    let response = 'unknown';
+    try {
+      response = await this.client.submitblock({
+        hexdata,
+      });
+      if (response == null) {
+        response = 'SUCCESS!';
+      }
+      console.log(`BLOCK SUBMISSION RESPONSE: ${response}`);
+      console.log(hexdata);
+      console.log(JSON.stringify(response));
+    } catch (e) {
+      response = e;
+      console.log(`BLOCK SUBMISSION RESPONSE ERROR: ${e}`);
     }
-
-    public async getBlockTemplate(blockHeight: number): Promise<IBlockTemplate> {
-        let result: IBlockTemplate;
-        try {
-            const block = await this.rpcBlockService.getBlock(blockHeight);
-
-            if (block != null && block.data != null) {
-                return Promise.resolve(JSON.parse(block.data));
-            } else if (block == null) {
-                if (process.env.NODE_APP_INSTANCE != null) {
-                    // There is a unique constraint on the block height so if another process tries to lock, it'll throw
-                    try {
-                        await this.rpcBlockService.lockBlock(
-                          blockHeight,
-                          process.env.NODE_APP_INSTANCE,
-                        );
-                    } catch (e) {
-                        result = await this.waitForBlock(blockHeight);
-                    }
-                }
-
-                while (!result) {
-                    await new Promise((r) => setTimeout(r, 100));
-
-                    try {
-                        result = await this.client.getblocktemplate({
-                            template_request: {
-                                rules: ['segwit'],
-                                mode: 'template',
-                                capabilities: ['serverlist', 'proposal'],
-                            },
-                        });
-                        await this.rpcBlockService.saveBlock(
-                          blockHeight,
-                          JSON.stringify(result),
-                        );
-                    } catch (e) {
-                        console.error('RETRY - getblocktemplate:', e.message);
-                    }
-                }
-            } else {
-                //wait for block
-                result = await this.waitForBlock(blockHeight);
-            }
-        } catch (e) {
-            console.error('Error getblocktemplate:', e.message);
-            throw new Error('Error getblocktemplate');
-        }
-        console.log(`getblocktemplate tx count: ${result.transactions.length}`);
-        return result;
-    }
-
-    public async getMiningInfo(): Promise<IMiningInfo> {
-        let miningInfo: IMiningInfo;
-        let counter = 0;
-        while (!miningInfo && counter < 5) {
-            await new Promise((r) => setTimeout(r, 100));
-            try {
-                miningInfo = await this.client.getmininginfo();
-            } catch (e) {
-                console.error('RETRY - getmininginfo: ', e.message);
-            }
-            counter++;
-        }
-        if (
-          !this.currentMiningInfo ||
-          miningInfo.blocks !== this.currentMiningInfo.blocks
-        ) {
-            this.currentMiningInfo = miningInfo;
-            console.log(`getMiningInfo: block height ${miningInfo.blocks}`);
-        }
-        return miningInfo;
-    }
-
-    public async SUBMIT_BLOCK(hexdata: string): Promise<string> {
-        let response = 'unknown';
-        try {
-            response = await this.client.submitblock({
-                hexdata,
-            });
-            if (response == null) {
-                response = 'SUCCESS!';
-            }
-            console.log(`BLOCK SUBMISSION RESPONSE: ${response}`);
-            console.log(hexdata);
-            console.log(JSON.stringify(response));
-        } catch (e) {
-            response = e;
-            console.log(`BLOCK SUBMISSION RESPONSE ERROR: ${e}`);
-        }
-        return response;
-    }
+    return response;
+  }
 }
